374eaba314b7364352fbe5601f76a44e3c9a8819
tjbcranleigh <tjb@cranleigh.org>
2016-08-01 13:36:56 +0100

    404 URI Tracking

--- 404.php
+++ 404.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>PICNIC</title>
+<title>404 Error</title>
 </head>
 
 <body>
 
-	<h2>You did it... well done ...you broke my webware...waaaaah</h2>
-
+	<h2>Wow, that's embarrassing ... something has gone wrong</h2>
+	
+	<p>Please go back and try again. If this continues to be a problem then please submit a helpdesk ticket with the steps that led to the error</p>
+	
+	<a href="http://help.cranleigh.org">Click Here to Visit Helpdesk and Submit a Ticket</a><br />
+	<a href="mailto:helpdesk@cranleigh.org">Click Here to Email Helpdesk and Submit a Ticket</a>
+	
+	<a href="http://signagemanager.cranleigh.org">Return to Signage Manager</a>
+	
+	<?php echo $_SERVER['REQUEST_URI']; ?>
+	
 </body>
 </html>
